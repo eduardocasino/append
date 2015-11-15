870c4c093f1cd2b6b3988e3548cabb9f2b0886d7
Eduardo Casino <mail@eduardocasino.es>
2015-11-15 19:20:09 +0100

    Convert to COM. Put tempDTA and part of cmdline in PSP. Remove unnecessary cs -> ds setting.

--- append.asm
+++ append.asm
@@ -18,17 +18,30 @@
 ; 04-06-01  casino_e@terra.es   First version
 ; 04-06-03  casino_e@terra.es   Add note to help stating that when /E is used,
 ;                               no paths that can appear on the command line.
-; 13-06-03  Eric Auer           Make older versions of nasm happy
+; 04-06-13  Eric Auer           Make older versions of nasm happy
+; 04-06-16  Eric Auer /
+;           casino_e@terra.es   Convert to COM
+; 04-06-17  casino_e@terra.es   Put tempDTA and part of cmdline in PSP
+; 04-06-18  casino_e@terra.es   Remove unnecessary cs -> ds setting
 ;
 
 %ifdef NEW_NASM
         cpu     8086
 %endif
 
-segment         code
 ; ===========================================================================
 ; RESIDENT PART
 ; ===========================================================================
+
+; PSP
+;
+; tempDTA	times 0x80 db 0
+; cmdline       times 0x100 db 0	; First 128 bytes of cmdline in PSP
+;
+                org     0x100
+                jmp     start		; Last 128 bytes of cmdline
+		times 0x7D db 0
+
 %include        "useful.mac"
 %include        "environ.asm"
 %include        "cmdline.asm"
@@ -37,7 +50,7 @@ segment         code
 
 cmd_id          db      6               ; 6, "APPEND"
 append_prefix   db      "APPEND="
-append_path     times 0x80 db 0
+append_path     times 0x100 db 0
 
 append_state    dw      0011000000000001b
 ;                       ||||\_________/|
@@ -58,7 +71,6 @@ APPEND_SRCHDRV  equ     0001000000000000b
 APPEND_SRCHPTH  equ     0010000000000000b
 APPEND_ENVIRON  equ     0100000000000000b
 APPEND_EXTENDD  equ     1000000000000000b
-
 
 NoAppend        db      13, "No Append", 13, 10, '$'
 Invalid         db      13, "Invalid switch  - ", '$'
@@ -101,15 +113,10 @@ Help    db      13, "FreeDOS APPEND. Enables programs to open data files in "
 end_resident:
 ; ================== END OF RESIDENT CODE ================================
 
+WrnInstalled    db      13, "APPEND already installed", 13, 10, '$'
+WrongAppend     db      13, "Incorrect APPEND version", 13, 10, '$'
 
-..start:
-                mov     ax, data
-                mov     ds, ax
-                mov     ax, stack
-                mov     ss, ax
-                mov     sp, stacktop
-
-                mov     ax, 0xB710      ; Check if we're already installed
+start:          mov     ax, 0xB710      ; Check if we're already installed
                 mov     dx, 0x0000
                 int     0x2F
 
@@ -133,8 +140,14 @@ installed:      mov     dx, WrnInstalled
 quit:           mov     ax, 0x4C01      ; Exit, errorlevel 1
                 int     0x21
 
-install:        mov     ah, 0x51        ; Get PSP pointer
-                int     0x21
+install:        mov     bx, cs
+
+		; points tempDTA to first 128 bytes of PSP and cmdline to last
+		; 128 bytes of PSP plus first 128 bytes of program. cmdline
+		; already contains 0x80
+		;
+		mov	[cs:tempDTA+2], bx
+		mov	[cs:cmdline+2], bx
 
                 call    get_environ     ; Get PARENT environment
                                         
@@ -148,26 +161,20 @@ install:        mov     ah, 0x51        ; Get PSP pointer
                 call    parse_cmds
                 jc      quit
                 
-                push    bx
-
                 ; Free some bytes, release environment
                 ;
-                mov     bx,[es:0x2C]    ; Segment of environment
-                mov     al,0x49         ; Free memory
-                mov     es,bx
+                mov     bx, [es:0x2C]    ; Segment of environment
+                mov     al, 0x49         ; Free memory
+                mov     es, bx
                 int     0x21
-
-                push    ds              ; Set DS for installing new handlers
-                push    cs
-                pop     ds
 
                 ; Get vect to original int2f handler
                 ;
                 mov     ax, 0x352F
                 int     0x21            ; get vector to ES:BX
                 mov     ax, es
-                mov     [cs:old_int2f], bx
-                mov     [cs:old_int2f+2], ax
+                mov     [old_int2f], bx
+                mov     [old_int2f+2], ax
 
                 ; Now, install new int2f handler
                 ;
@@ -180,8 +187,8 @@ install:        mov     ah, 0x51        ; Get PSP pointer
                 mov     ax, 0x3521
                 int     0x21            ; get vector to ES:BX
                 mov     ax, es
-                mov     [cs:old_int21], bx
-                mov     [cs:old_int21+2], ax
+                mov     [old_int21], bx
+                mov     [old_int21+2], ax
 
                 ; Now, install new int21 handler
                 ;
@@ -189,34 +196,15 @@ install:        mov     ah, 0x51        ; Get PSP pointer
                 mov     dx, int21
                 int     0x21            ; DS:DX -> new interrupt handler
 
-                pop     ds              ; Restore DS
-
-                mov     byte [cs:p_flags], RESIDENT     ; Set resident flag
-                                                        ; and clean the rest
+                mov     byte [p_flags], RESIDENT     ; Set resident flag
+                                                     ; and clean the rest
 
                 ; Terminate and stay resident
                 ;
-                pop     bx
                 mov     dx, end_resident+15
                 mov     cl, 4
                 shr     dx, cl          ; Convert to paragraphs
-                push    cs
-                pop     cx
-                add     dx, cx
-                sub     dx, bx          ;  (CS - PSP) + ((res. offset + 15)/4)
 
                 mov     ax, 0x3100      ; Errorlevel 0
                 int     0x21
 
-
-segment         data
-
-WrnInstalled    db      13, "APPEND already installed", 13, 10, '$'
-WrongAppend     db      13, "Incorrect APPEND version", 13, 10, '$'
-
-
-segment         stack   stack
-
-                resb    256
-
-stacktop:
